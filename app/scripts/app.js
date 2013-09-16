780b960cb2678851c631a343bb3847e4ad66d99f
Jakob Viskum Damgaard <jakob_damgaard@hotmail.com>
2013-09-16 23:22:41 +0200

    First slides with animation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,5 +2,18 @@
 define([], function () {
     'use strict';
 
-    return '\'Allo \'Allo!';
+    var sectionHeight = $(window).height();
+
+    $('#home article').height(sectionHeight);
+
+    $('#home-hi').addClass('active');
+
+    $(document).on('scroll', function () {
+        var pos = $(document).scrollTop();
+
+        $('header').toggleClass('active', pos > 0.5 * sectionHeight);
+        $('#home-hi').toggleClass('active', pos <= 0.5 * sectionHeight);
+        $('#home-exploring').toggleClass('active', pos > 0.5 * sectionHeight && pos <= 1.5 * sectionHeight);
+
+    });
 });
