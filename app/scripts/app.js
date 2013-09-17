96366e380c5789521d7009a1c22b447176bea0e4
Jakob Viskum Damgaard <jakob_damgaard@hotmail.com>
2013-09-17 22:43:33 +0200

    Navigation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,16 +4,35 @@ define([], function () {
 
     var sectionHeight = $(window).height();
 
-    $('#home article').height(sectionHeight);
-
-    $('#home-hi').addClass('active');
-
-    $(document).on('scroll', function () {
-        var pos = $(document).scrollTop();
-
-        $('header').toggleClass('active', pos > 0.5 * sectionHeight);
-        $('#home-hi').toggleClass('active', pos <= 0.5 * sectionHeight);
-        $('#home-exploring').toggleClass('active', pos > 0.5 * sectionHeight && pos <= 1.5 * sectionHeight);
-
+    $(function () {
+        initialize();
     });
+
+    var initialize = function () {
+
+        // Loading finished
+        $('body').addClass('loaded');
+
+        // Adjust based on window height
+        $('#home article').height(sectionHeight);
+        $('header').css({
+            top: sectionHeight + 'px'
+        });
+
+        // Activate first slide
+        $('#home-hi').addClass('active');
+
+        $(document).on('scroll', function () {
+            var pos = $(document).scrollTop();
+
+            // Header
+            $('header').toggleClass('active', pos > 0);
+            $('header').toggleClass('fixed', pos > 1 * sectionHeight);
+
+            // Home
+            $('#home-hi').toggleClass('active', pos <= 0.5 * sectionHeight);
+            $('#home-exploring').toggleClass('active', pos > 0.5 * sectionHeight && pos <= 1.5 * sectionHeight);
+
+        });
+    };
 });
