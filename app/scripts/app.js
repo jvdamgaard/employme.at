aed77815a136929bacd2df74700915c530faee82
Jakob Viskum Damgaard <jakob_damgaard@hotmail.com>
2013-09-19 23:10:05 +0200

    Optimized font, basic typography, scroll control, portrait image

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,34 +2,99 @@
 define([], function () {
     'use strict';
 
-    var sectionHeight = $(window).height();
+    var windowHeight;
 
+    /**
+     * Initialize app
+     * @return {void}
+     */
     var initialize = function () {
 
-        // Adjust based on window height
-        $('#home article').height(sectionHeight);
-        $('header').css({
-            top: sectionHeight + 'px'
-        });
-
-        // Activate first slide
-        $('#home-hi').addClass('active');
-
-        $(document).on('scroll', function () {
-            var pos = $(document).scrollTop();
-
-            // Header
-            $('header').toggleClass('active', pos > 0);
-            $('header').toggleClass('fixed', pos > 1 * sectionHeight);
-
-            // Home
-            $('#home-hi').toggleClass('active', pos <= 0.5 * sectionHeight);
-            $('#home-exploring').toggleClass('active', pos > 0.5 * sectionHeight && pos <= 1.5 * sectionHeight);
-
-        });
+        setSectionsHeight();
 
         // Loading and initilazition finished
         $('body').addClass('loaded');
+
+        // Activate first slide
+        $('#home-hi').addClass('active');
+    };
+
+
+    /**
+     * Global events
+     */
+
+    $(window).on('resize orientationchange', function () {
+        windowHeight = $(window).height();
+        setSectionsHeight();
+        addjustControllers();
+    });
+
+    $(document).on('scroll', function () {
+        addjustControllers();
+    });
+
+    /**
+     * Adjust sections based on scroll position
+     * @return {void}
+     */
+    var addjustControllers = function () {
+        var posTop = $(document).scrollTop();
+
+        var activate = function (selector) {
+            activateElement(selector, posTop);
+        };
+
+        // Header
+        var $header = $('header');
+        $header.toggleClass('active', posTop > 0);
+        $header.toggleClass('fixed', posTop > windowHeight);
+        $('#portrait-image .image').css({
+            height: (windowHeight / 2 - posTop) / windowHeight * 2 * 100 + '%'
+        });
+
+        // Home
+        activate('#home-hi');
+        activate('#home-exploring');
+
+        // Test
+        activate('#test');
+
+    };
+
+    var activateElement = function (selector, posTop) {
+        var $el = $(selector);
+
+        if ($el.length === 0) {
+            return false;
+        }
+
+        var posBottom = posTop + windowHeight;
+        var offsetTop = $el.offset().top;
+        var height = $el.height();
+        var topMiddle = offsetTop + (windowHeight / 2);
+        var bottomMiddle = offsetTop + height - (windowHeight / 2);
+
+        var activate = (posBottom > topMiddle && posTop < bottomMiddle);
+
+        $el.toggleClass('active', activate);
+
+        return true;
+    };
+
+    /**
+     * Set all sections heights based on window height
+     */
+    var setSectionsHeight = function () {
+        windowHeight = $(window).height();
+
+        $('header').css({
+            top: windowHeight + 'px'
+        });
+
+        $('#home article').css({
+            height: windowHeight
+        });
     };
 
     initialize();
