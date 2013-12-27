498077fa6a84c2d700434eb5860fbbb43f5298b7
jvdamgaard <jakob.viskum.damgaard@gmail.com>
2013-12-27 21:34:17 +0100

    Method for logging library versions

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,35 +4,45 @@
  * - logs version of dependencies
  */
 
-/**
- * # Dependencies
- */
-
+// Dependencies
 var _ = require('lodash');
 var $ = require('jquery');
 var moment = require('moment');
 var app = require('app');
 
+/**
+ * Log version of dependencies with beautified formatting
+ *
+ * Example:
+ *
+ *     logVersion('Lo-Dash', _.VERSION);
+ *
+ * @param  {string} name    Name og librariry/dependency
+ * @param  {string} version Version number in format {major}.{minor}.{patch}
+ * @return {void}
+ */
+var logVersion = function (name, version) {
+    var charsBeforeSeperator = 25;
+    var content = ' - ' + name;
+    content += new Array(charsBeforeSeperator - content.length).join(' ');
+    content += ': ' + version;
+    console.log(content);
+};
 
 /**
- * Holds the name and version number og the website
- * @type {String}
+ * Log a seperator with lines
+ * @return {void}
  */
-var appVersion = ' ' + app.name;
-for (var i = 24 - app.name.length - 1; i >= 0; i--) {
-    appVersion += ' ';
-}
-appVersion += ': ' + app.version;
+var logSeperator = function () {
+    console.log('------------------------------------------');
+};
 
-
-/**
- * Log versions of dependencies
- */
-console.log('----------------------------------------------------------------');
-console.log(appVersion);
-console.log('----------------------------------------------------------------');
-console.log(' - jQuery                : ' + $.fn.jquery);
-console.log(' - Modernizr             : ' + Modernizr._version);
-console.log(' - Lo-Dash               : ' + _.VERSION);
-console.log(' - Moment.js             : ' + moment.version);
-console.log('-----------------------------------------------------------------');
+// Log versions of dependencies
+logSeperator();
+logVersion(app.name, app.version);
+logSeperator();
+logVersion('jQuery', $.fn.jquery);
+logVersion('Modernizr', Modernizr._version);
+logVersion('Lo-Dash', _.VERSION);
+logVersion('Moment.js', moment.version);
+logSeperator();
