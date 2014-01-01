eaa847cbdcd2670a6db7f18dfdade522a70daa81
jvdamgaard <jakob@jakobs-imac.home>
2014-01-01 21:33:55 +0100

    Better exampel

--- app/scripts/util/namespace.js
+++ app/scripts/util/namespace.js
@@ -11,19 +11,19 @@
  *
  * Examples:
  *
- * ```
- * require('./util/namespace')('App.my.module');
  *
- * App.my.module = {
- *     name: 'Jakob',
- *     birthYear: 1983
- * };
- * ```
+ *     require('./util/namespace')('App.my.module');
  *
- * ```
- * var namespace = require('./util/namespace');
- * var moduleAttachedToApp = namespace('another.module', App);
- * ```
+ *     App.my.module = {
+ *         name: 'Jakob',
+ *         birthYear: 1983
+ *     };
+ *
+ *     // ---------------
+ *
+ *     var namespace = require('./util/namespace');
+ *     var moduleAttachedToApp = namespace('another.module', App);
+ *     console.log(App.another.module === moduleAttachedToApp); // true
  *
  * @param  {string} name dot seperated string with the namespace
  * @param  {object} root object to attach namespace to. Defaults to ´window´
