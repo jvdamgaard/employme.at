ffc553b99750e4cc9deddc2350505a459f6ddbd9
jvdamgaard <jakob.viskum.damgaard@gmail.com>
2014-01-12 01:03:19 +0100

    clean up before deploying

---
+++ grunt/options/clean.js
@@ -0,0 +1,19 @@
+/**
+ * Clean files and folders
+ */
+
+module.exports = {
+    build: {
+        src: [
+            '.tmp/bower_components',
+            '.tmp/coverage',
+            '.tmp/dist/scripts/modernizr.js',
+            '.tmp/sass',
+            '.tmp/tests',
+            'app',
+            'docs',
+            'grunt',
+            'test'
+        ]
+    },
+};
